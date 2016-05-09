a6c4295ae57e37b968fee486d5271417802b0539
Higbee-Keith <hig12012@byui.edu>
2016-05-09 15:25:13 -0700

    Created HelloWorld file
    basic file here

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * @(#)HelloWorld.java
+ *
+ *
+ * @Keith Higbee 
+ * @version 1.00 2016/5/9
+ */
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+    	HelloWorld hw = new HelloWorld();
+    	hw.display();
+    }
+    
+    public void display() {
+    	System.out.println("Hello World!");
+    }
+    
+
+}
